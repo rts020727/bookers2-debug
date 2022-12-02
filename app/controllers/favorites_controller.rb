fc58b7c32d9b2ea0ae832729e6b7a055104ba110
rts020727 <rts020727@gmail.com>
2022-12-02 07:54:34 +0000

    add favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,12 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
   end
-  
+
   def destroy
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.find_by(user_id: current_user.id)
