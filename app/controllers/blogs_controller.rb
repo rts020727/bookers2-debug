26ba9eae93b160a9391ea193e3de9250f0342c77
rts020727 <rts020727@gmail.com>
2023-01-05 13:56:23 +0000

    カレンダー追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,25 @@
 class BlogsController < ApplicationController
+
   def index
+    @blogs = Blog.all
+    @blog = Blog.new
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def create
+    Blog.create(blog_params)
+    redirect_to blogs_path
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content, :start_time)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ Rails.application.routes.draw do
   end
   
   resources :maps 
+  
+  resources :blogs
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
